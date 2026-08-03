97be3162e840e5e246c7d86c33e8972c32421cfb
IWABUCHI Yuki butchi <butchiyu+github@gmail.com>
2026-08-03 10:36:25 +0900

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,5 +8,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="ja"><body>{children}</body></html>;
+  return <html lang="ja">
+    <head>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P84T2BRG');`,
+        }}
+      />
+    </head>
+    <body>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P84T2BRG" height="0" width="0" style={{ display: "none", visibility: "hidden" }} title="Google Tag Manager" /></noscript>
+      {children}
+    </body>
+  </html>;
 }
