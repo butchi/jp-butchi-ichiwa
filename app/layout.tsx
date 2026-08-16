2337b00c5ff4ade3fbb6753cb473bf5f018ace05
IWABUCHI Yuki butchi <butchiyu+github@gmail.com>
2026-08-17 05:55:24 +0900

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -11,14 +12,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return <html lang="ja">
     <head>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2862407789219932" crossOrigin="anonymous" />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P84T2BRG');`,
-        }}
-      />
     </head>
     <body>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P84T2BRG" height="0" width="0" style={{ display: "none", visibility: "hidden" }} title="Google Tag Manager" /></noscript>
+      <GoogleTagManager gtmId="GTM-P84T2BRG" />
       {children}
     </body>
   </html>;
